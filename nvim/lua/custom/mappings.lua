ea477432b97b55c9fed531b757608c695956d02e
aritroCoder <ogcodinggod@gmail.com>
2025-05-16 08:02:35 +0000

    removed mappings for arrow keys
    Signed-off-by: aritroCoder <ogcodinggod@gmail.com>

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -1,11 +1,16 @@
 local M = {}
 
 M.general = {
-  n = {
-    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
-    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
-    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
-    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
-  },
+    n = {
+        ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+        ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+        ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+        ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+        -- Disable arrow keys
+        ["<Up>"] = { "<Nop>", "disable up" },
+        ["<Down>"] = { "<Nop>", "disable down" },
+        ["<Left>"] = { "<Nop>", "disable left" },
+        ["<Right>"] = { "<Nop>", "disable right" },
+    },
 }
 return M
